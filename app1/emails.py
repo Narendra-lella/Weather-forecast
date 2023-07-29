2efeba3c41a579e10e240564a10b64c28e9458bf
Narendra Lella <jhon.496.wick@gmail.com>
2023-07-29 14:10:23 +0530

    version-3

--- app1/emails.py
+++ app1/emails.py
@@ -5,7 +5,7 @@ from django.conf import settings
 def send_emails(email, token):
 
     subject = ' Conform Login'
-    message = f'Conform your account clicking on the link: http://127.0.0.1:8000/homepage/'
+    message = 'Conform your account clicking on the link: http://127.0.0.1:8000/homepage/'
     email_from = settings.EMAIL_HOST_USER
     recipient_list = []
     send_mail(subject , message, email_from, recipient_list)
--- app1/views.py
+++ app1/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 import requests
 from .models import  WeatherData
 from django.contrib.auth.models import User
-import uuid          #for creating unique tokens
+import uuid          
 from .emails import send_emails
 # Create your views here.
 
@@ -40,7 +40,7 @@ def register(request):
     return render(request, 'register.html')
 
 
-def page1(request):  # sourcery skip: use-named-expression
+def page1(request):    
 
     if request.method == "POST":
         username = request.POST['username']
@@ -57,29 +57,33 @@ def page1(request):  # sourcery skip: use-named-expression
             return redirect('/')
     return render(request, 'login.html')
 
-def mainpage(request):
+def mainpage(request):  # sourcery skip: extract-method, remove-unnecessary-else
     city = request.GET.get('city')
     #api = '895098b5d8379f2490a2922a5ca07143'
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=895098b5d8379f2490a2922a5ca07143'
     data = requests.get(url).json()
-    weather_data = WeatherData(
-        description=data['weather'][0]['description'],
-        temperature=data['main']['temp'],
-        humidity=data['main']['humidity'],
-        feels_like=data['main']['feels_like'],
-        location=data['name'],
-        datetime=timezone.now()
-    )
-    weather_data.save()
-    details = {
-        'location' : data['name'],
-        'temperature_in_k' : data['main']['temp'],
-        'temperature_in_c' : int(data['main']['temp']-273),
-        'humidity': data['main']['humidity'],
-        'feels_like': data['main']['feels_like'],
-        'description': data['weather'][0]['description']
-    }
-
-    context = {'data' : details }
-    print(context)
-    return render(request, 'mainpage.html',context)
+    if data.get('cod') == 200:
+        weather_data = WeatherData(
+            description=data['weather'][0]['description'],
+            temperature=data['main']['temp'],
+            humidity=data['main']['humidity'],
+            feels_like=data['main']['feels_like'],
+            location=data['name'],
+            datetime=timezone.now()
+        )
+        weather_data.save()
+        details = {
+            'location' : data['name'],
+            'temperature_in_k' : data['main']['temp'],
+            'temperature_in_c' : int(data['main']['temp']-273),
+            'humidity': data['main']['humidity'],
+            'feels_like': data['main']['feels_like'],
+            'description': data['weather'][0]['description']
+        }
+        context = {'data' : details }
+        print(context)
+        return render(request, 'mainpage.html',context)
+    else:
+        messages.error(request, "Please enter proper loction")
+        return render(request, "mainpage.html")
+        
